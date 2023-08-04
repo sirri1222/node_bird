9ee66fd3e3b85237af28e9f25c8445db5aaf7845
sirri1222 <hyeonji159753@gmail.com>
2023-08-04 17:08:14 +0900

    userprofile 컴포넌트 수정

--- prepare/front/components/UserProfile.js
+++ prepare/front/components/UserProfile.js
@@ -1,5 +1,24 @@
-const UserProfile = ()=>{
-    return <div>유저프로필</div>
+import { Avatar, Card } from "antd";
+import {useCallback} from 'react'
+const UserProfile = ({setisLoggedIn})=>{
+
+    const onLogOut = useCallback(()=>{
+        setisLoggedIn(false)
+    },[])
+    return <Card
+    actions={
+        [
+            <div key="twit">짹짹<br />0</div>,
+            <div key="followings">팔로잉<br />0</div>,
+            <div key="folloer">팔로워<br />0</div>
+        ]
+    }
+    > 
+    <Card.Meta
+    avatar={<Avatar>hyunji</Avatar>}
+    title="HYUNJI SON" />
+        <Button onClick={onLogOut}>로그아웃</Button>
+        </Card>
 }
 
 export default UserProfile;
