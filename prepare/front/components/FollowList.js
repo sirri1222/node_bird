b3a0c2d8f0e0d5d2b45d0852f582b193789eac47
sirri1222 <hyeonji159753@gmail.com>
2023-08-07 13:52:30 +0900

    구조 수정 및 프로필 페이지 진행중

--- prepare/front/components/FollowList.js
+++ prepare/front/components/FollowList.js
@@ -1,5 +1,21 @@
+import { Button, List } from "antd";
+
+List
 const FollowList = ({header, data})=>{
     return
+    <List 
+    size="small"
+    style={{marginBottom: 20}}
+    grid={{gutter: 4,xs:2, md:3}}
+    header={<div>header</div>}
+    loadMore={<div style={{textAlign: 'center', margin:'10px 0'}}
+    bordered
+dataSource = {data}
+renderItem = {(item)=>(
+    <List.Item style={{marginTop:20}}></List.Item>
+)}
+><Button>더보기</Button> </div>} />
+
 }
 
 export default FollowList;
