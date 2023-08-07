e070c6008e94dd16da6e350b1c75cb043e79426e
sirri1222 <hyeonji159753@gmail.com>
2023-08-07 22:37:13 +0900

    redux 설치 및 store 생성

---
+++ prepare/front/store/configureStore.js
@@ -0,0 +1,8 @@
+const {createWrapper} = require('next-redux-wrapper')
+
+const configureStore = () =>{
+
+};
+const wrapper= createWrapper(configureStore, {
+    debug:process.env.NODE_ENV === 'development'
+})
