0846f137f8efb2c5234c24745ee4276a14add61d
sirri1222 <hyeonji159753@gmail.com>
2023-08-09 18:21:51 +0900

    포스트 폼 만들기 수정

--- prepare/front/components/PostForm.js
+++ prepare/front/components/PostForm.js
@@ -1,20 +1,26 @@
 import { Button, Form } from "antd";
 import useSelection from "antd/lib/table/hooks/useSelection";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addPost } from "../reducers/post";
 
 const PostForm = () => {
   const { imagePaths } = useSelector((state) => state.post);
   const dispatch = useDispatch();
-
+  //   실제 도메에 접근하기위해 ref를 사용
+  const imageInput = useRef();
   const [text, setText] = useState("");
   const onChangeText = useCallback((e) => {
     setText(e.target.value);
   }, []);
   const onSubmit = useCallback(() => {
     dispatch(addPost);
+    setText("");
   }, []);
+
+  const onClickImageInput = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
   return (
     <Form
       style={{ margin: "10px 0 20px" }}
@@ -28,8 +34,8 @@ const PostForm = () => {
         placeholder="어떤일이 있었나요?"
       />
       <div>
-        <input />
-        <Button>이미지 업로드</Button>
+        <input type="file" multiple hidden ref={imageInput} />
+        <Button onClick={onClickImageInput}>이미지 업로드</Button>
         <Button type="primary" style={{ float: "right" }} htmlType="submit">
           짹짹
         </Button>
